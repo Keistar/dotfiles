e83b22f1d01d428ce19e21be42afed000fa72d65
k2 <keistar0926@gmail.com>
2024-06-22 22:39:12 +0900

    config

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,4 +1,23 @@
 local map = LazyVim.safe_keymap_set
+local opts = { noremap = true, silent = true }
+
+-- Increment/Decrement
+map("n", "+", "<C-a>")
+map("n", "-", "<C-x>")
+
+-- Select all
+map("n", "<C-a>", "ggVG")
 
 -- jj to Esc
 map("i", "jj", "<Esc>")
+
+-- Save file
+map("n", "<Leader>w", ":update<Return>", opts)
+map("n", "<Leader>q", ":quit<Return>", opts)
+map("n", "<Leader>Q", ":qa<Return>", opts)
+
+-- Tabs
+map("n", "te", ":tabedit")
+map("n", "<tab>", ":tabnext<Return>", opts)
+map("n", "<S-tab>", ":tabprev<Return>", opts)
+map("n", "tw", ":tabclose<Return>", opts)
