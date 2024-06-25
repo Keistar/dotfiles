ad62f4eb2fd625c8473a598cc5b0f9ad8147e090
k2 <keistar0926@gmail.com>
2024-06-26 00:38:01 +0900

    setting config

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -6,7 +6,7 @@ if wezterm.config_builder then
   config = wezterm.config_builder()
 end
 
-config.color_scheme = 'AdventureTime'
+config.color_scheme = 'Batman'
 -- 背景透過
 config.window_background_opacity = 0.90
 
@@ -30,6 +30,7 @@ config.keys = {
 -- フォントの設定
 -- config.font = wezterm.font("好きなフォント", {weight="Medium", stretch="Normal", style="Normal"})
 config.font = wezterm.font("Symbols Nerd Font Mono", {weight="Regular", stretch="Normal", style="Normal"})
+config.cell_width = 0.6
 -- フォントサイズの設定
 config.font_size = 16
 
