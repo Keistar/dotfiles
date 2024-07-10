05f9ae2db59cc1e1ff16bca922cda2608b31367c
k2 <keistar0926@gmail.com>
2024-07-10 22:35:48 +0900

    vueファイルのformatter実装

--- nvim/lua/plugins/formatter.lua
+++ nvim/lua/plugins/formatter.lua
@@ -2,11 +2,20 @@ return {
   "stevearc/conform.nvim",
   dependencies = { "mason.nvim" },
   opts = {
-    lua = { "stylua" },
-    -- Conform will run multiple formatters sequentially
-    python = { "isort", "black" },
-    -- Use a sub-list to run only the first available formatter
-    javascript = { { "prettierd", "prettier" } },
-    php = { { "php_cs_fixer", "phpcbf", "phpinsights" } },
+    formatters_by_ft = {
+      lua = { "stylua" },
+      -- Conform will run multiple formatters sequentially
+      python = { "isort", "black" },
+      -- Use a sub-list to run only the first available formatter
+      javascript = { { "prettierd", "prettier" } },
+      typescript = { { "prettierd", "prettier" } },
+      vue = { { "prettierd", "prettier" } },
+      php = { { "php_cs_fixer", "phpcbf", "phpinsights" } },
+    },
+    format_on_save = {
+      -- I recommend these options. See :help conform.format for details.
+      lsp_format = "fallback",
+      timeout_ms = 500,
+    },
   },
 }
