cd56eeea72c148f94e118e76d2520352d4a23b4e
k2 <keistar0926@gmail.com>
2024-07-11 23:06:33 +0900

    update link shell

--- ln_nvim_p.sh
+++ ln_nvim_p.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
+cmd="nvim"
 
-target="${HOME}/.config/nvim"
+target_1="${HOME}/.config/${cmd}"
+target_2="${HOME}/.local/share/${cmd}"
+target_3="${HOME}/.local/state/${cmd}"
+target_4="${HOME}/.cache/${cmd}"
 base="$(pwd)/nvim_p"
 
-rm -rf ${target}
+rm -rf ${target_1} ${target_2} ${target_3} ${target_4}
 ln -s ${base} ${target}
