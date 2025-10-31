62ceba0c825a064c5899527c6fde800867c8a37e
Keisuke Koshino <k2@KeisukenoMacBook-Pro.local>
2025-10-31 10:03:28 +0900

    fix

--- nvim/.config/nvim/lua/config/keymap.lua
+++ nvim/.config/nvim/lua/config/keymap.lua
@@ -6,19 +6,19 @@ vim.g.mapleader = ','
 vim.keymap.set("i", "jj", "<esc>")
 vim.keymap.set("i", "jk", "<esc>:w")
 
-set showbreak=↪
+-- set showbreak=↪
 
-nnoremap <Down> gj
-nnoremap <Up>   gk
-nnoremap あ a
-nnoremap い i
-nnoremap う u
-nnoremap お o
-nnoremap っd dd
-nnoremap っy yy
+-- nnoremap <Down> gj
+-- nnoremap <Up>   gk
+-- nnoremap あ a
+-- nnoremap い i
+-- nnoremap う u
+-- nnoremap お o
+-- nnoremap っd dd
+-- nnoremap っy yy
 
-inoremap <C-j> <Down>
-inoremap <C-k> <Up>
-inoremap <C-h> <Left>
-inoremap <C-l> <Right>
+-- inoremap <C-j> <Down>
+-- inoremap <C-k> <Up>
+-- inoremap <C-h> <Left>
+-- inoremap <C-l> <Right>
 
