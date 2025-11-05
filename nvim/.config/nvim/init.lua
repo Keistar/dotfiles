7c75790b8d68364ef5494eb88b7a8c442f54caf0
Keisuke Koshino <k2@KeisukenoMacBook-Pro.local>
2025-11-06 01:15:46 +0900

    Fix to LazyVim

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,4 +1,2 @@
-require("config.keymap")
+-- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
-require("config.options")
-
--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -1,19 +1,3 @@
-local opt = vim.opt
-
--- 行番号の表示
-opt.number = true
-opt.relativenumber = true
-
--- タブとインデントの設定
-opt.tabstop = 4
-opt.shiftwidth = 4
-opt.expandtab = true
-opt.smartindent = true
-opt.autoindent = true
-
--- 検索設定
-opt.ignorecase = true
-opt.smartcase = true
-
--- その他
-
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
