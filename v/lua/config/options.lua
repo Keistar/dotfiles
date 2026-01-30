81b0af3fc2cf4360151581fc7f93be30000bde99
en-koshino <keisuke_koshino@en-japan.com>
2026-01-30 17:28:57 +0900

    add neovim config v

---
+++ v/lua/config/options.lua
@@ -0,0 +1,16 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+
+vim.opt.list = true
+vim.opt.listchars = {
+  space = "·",
+  tab = "→ ",
+  trail = "•",
+  nbsp = "␣",
+}
+
+vim.cmd([[
+  highlight ZenkakuSpace guibg=#3c0000
+  match ZenkakuSpace /　/
+]])
